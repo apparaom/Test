0241cbfc6cc84fb7a6521f11bb89c4b2fbedaa9a
lnc <lnc@DIN76000165.corp.capgemini.com>
2017-06-14 19:50:24 +0530

    test

--- GitTest/src/org/cap/tst/Test.java
+++ GitTest/src/org/cap/tst/Test.java
@@ -5,9 +5,9 @@ public class Test {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println(":::::::::::::");
-		System.out.print(";;;;;;;;;");
-		System.out.println(":::::::::::::");
-		System.out.println(":::::::::::::");
+		System.out.print(";;;;1234;;;;;");
+		System.out.println("::::1q234:::::::::");
+		System.out.println(":::::1aaa2334::::::::");
 		System.out.print(";;;;;;;;;");
 	}
 
